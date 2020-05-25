0e6b8ab01ceb7b16fcb15852fe3ccb19b5bab983
janikwibilea <janik.wehrli@bbz-sh.ch>
2020-05-25 20:43:14 +0200

    a

---
+++ tag3/7.3.php
@@ -0,0 +1,18 @@
+<?php 
+
+// $myip = "127.0.0.1"; localhost
+
+
+//IPv6 localhost = ::1
+$myip = "::1";
+
+$remoteaddr = $_SERVER["REMOTE_ADDR"];
+
+if($myip == $remoteaddr){
+    echo "„Sie waren heute bereits hier! Kommen Sie später wieder!“";
+}else{
+    echo "„<br>Guten Tag; hier der Seiten Inhalt: ...“ ";
+}
+
+
+?>
